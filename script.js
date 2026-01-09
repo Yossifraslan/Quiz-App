c73f382bd35a738029b0a6668345586ecf5a9777
Yossifraslan <raslanyossif@gmail.com>
2026-01-09 10:28:22 +0800

    Initial Handle next button function.

--- script.js
+++ script.js
@@ -95,4 +95,21 @@ function selectAnswer(e){
     nextButton.style.display = "block";
 }
 
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
+
 startQuiz();
