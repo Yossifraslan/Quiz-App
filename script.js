76122ec8f60ef20c17ad4b282d13f11337fa1ae3
Yossifraslan <raslanyossif@gmail.com>
2026-01-09 10:05:42 +0800

    Initial show question function.

--- script.js
+++ script.js
@@ -51,3 +51,21 @@ function startQuiz(){
     nextButton.innerHTML = "Next";
     showQuestion();
 }
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+    });
+}
+
+
+
+startQuiz();
