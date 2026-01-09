2c860b5f77afa77d4aa502d4363734862f2f9018
Yossifraslan <raslanyossif@gmail.com>
2026-01-09 10:34:40 +0800

    Initial showScore function.

--- script.js
+++ script.js
@@ -95,7 +95,12 @@ function selectAnswer(e){
     nextButton.style.display = "block";
 }
 
-
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
 
 function handleNextButton(){
     currentQuestionIndex++;
